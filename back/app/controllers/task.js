7fd800a54231664a61d014ff237c64b9d7c5866c
antoine <antoine.lefrancq.manul@gmail.com>
2022-08-01 09:23:38 +0200

    router initialisé

--- back/app/controllers/task.js
+++ back/app/controllers/task.js
@@ -5,7 +5,19 @@ const taskController = {
     listTasks: async function (req, res) {
         // Récupérer la liste des taches
         // Renvoyer la liste des taches en json
-    }
+    },
+
+    create:async ()=>{
+
+    },
+
+    update: async()=>{
+
+    },
+
+    delete: async()=>{
+
+    },
 };
 
 module.exports = taskController;
--- back/app/router.js
+++ back/app/router.js
@@ -4,11 +4,10 @@ const taskController = require('./controllers/task');
 
 // Route pour la liste des taches
 router.get('/tasks', taskController.listTasks);
-
 // Route pour ajouter une tache
-
+router.post('/tasks', taskController.create)
 // Route pour modifier une tache
-
+router.put('/tasks/:id',taskController.update)
 // Route pour supprimer une tache
-
+router.delete('/tasks/:id',taskController.delete)
 module.exports = router;
